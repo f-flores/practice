8f3df15b0305fbc5a4ecfab1d6f74c8526b1bd35
Fabian Flores <ffflores1@outlook.com>
2018-08-09 10:34:51 -0400

    most frequent number in list of arrays

---
+++ problem-solving/mostFrequent.js
@@ -0,0 +1,35 @@
+/*
+
+  Given an array of integers, return the integer appearing the most frequently
+
+
+*/
+
+const arrInts = [3, 1, 2, 3, 4, 5, 4, 2, 4, 99, 99, 99, 23, 99, 23, 99, 2, 99, 0, 99];
+
+function mostFrequent(arr) {
+  let freqTable = new Object();
+  let maxCtr = 0, maxElem = 0;
+
+  for (elem of arr) {
+    // initialize occurrences of element to one when number is encountered for the first time
+    if (!freqTable[elem]) {
+      freqTable[elem] = 1;
+    } else {
+      // freqTable[elem] exists increment value
+      freqTable[elem] += 1;
+    }
+    if (freqTable[elem] > maxCtr) {
+      maxCtr = freqTable[elem];
+      maxElem = elem;
+    }
+  }
+  console.log("freqTable: ", JSON.stringify(freqTable, null, 2));
+  return maxElem;
+}
+
+
+
+
+console.log(mostFrequent(arrInts));
+
