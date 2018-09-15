da9f5d4aac687de2121047d097c31fc3f606ef4f
Fabian Flores <ffflores1@outlook.com>
2018-09-14 22:01:00 -0400

    changed function

--- problem-solving/newYearChaos.js
+++ problem-solving/newYearChaos.js
@@ -1,5 +1,4 @@
 /*
-
 It's New Year's Day and everyone's in line for the Wonderland rollercoaster ride! There are a number of people queued up, and each person wears a sticker indicating their initial position in the queue. Initial positions increment by  from  at the front of the line to  at the back.
 
 Any person in the queue can bribe the person directly in front of them to swap positions. If two people swap positions, they still wear the same sticker denoting their original places in line. One person can bribe at most two others. For example, if and  bribes , the queue will look like this: .
