06a121409f87dac36676180f1e21045e27efa88a
Fabian Flores <ffflores1@outlook.com>
2018-10-29 06:11:33 -0400

    updated FranchiseInfo

--- materialuisample/FranchiseInfo.js
+++ materialuisample/FranchiseInfo.js
@@ -1,26 +1,24 @@
-
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {MinAddressLength, MaxAddressLength, MinCompanyLength, MaxCompanyLength,
+        MinPhoneLength, MaxPhoneLength} from '../../constants/Consts';
+import API from '../../utils/myAPI';
+import FranchiseCreatedModal from './FranchiseCreatedModal';
 
 // @material-ui/core components
 import { withStyles } from '@material-ui/core/styles';
 import InputAdornment from "@material-ui/core/InputAdornment";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Icon from "@material-ui/core/Icon";
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 // @material-ui icons
 import Business from '@material-ui/icons/Business';
 import ContactMail from "@material-ui/icons/ContactMail";
 import ContactPhone from "@material-ui/icons/ContactPhone";
 import Email from "@material-ui/icons/Email";
-import Check from "@material-ui/icons/Check";
 // core components
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
-import CustomInput from 'components/CustomInput/CustomInput.jsx';
 import Button from "components/CustomButtons/Button.jsx";
-import signupPageStyle from "assets/jss/material-kit-pro-react/views/signupPageStyle.jsx";
+import FranchiseUpdateScreen from './FranchiseUpdateScreen';
 
 
 const styles = theme => ({
@@ -29,13 +27,23 @@ const styles = theme => ({
     width: '100%',
     backgroundColor: theme.palette.background.paper,
   },
+  container: {
+    display: 'grid',
+    gridGap: `${theme.spacing.unit * 2}px`,
+  },
+  vSpace: {
+    marginBottom: `${theme.spacing.unit * 4}px`,
+  },
 });
 
 class FranchiseInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      checked: [1],
+      doesFranchiseExist: false,
+      submittingFranchise: false,
+      isModalUpdated: false,
+      franchiseInfoUpdated: false,
       franchise: {
         name: "",
         address: "",
@@ -43,160 +51,241 @@ class FranchiseInfo extends Component {
         email: "",
       }
     };
-    this.handleToggle = this.handleToggle.bind(this);
+    this.checkIfFranchiseExists = this.checkIfFranchiseExists.bind(this);
+    this.getFranchiseInfo = this.getFranchiseInfo.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleToggle(value) {
-    const { checked } = this.state;
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    this.setState({
-      checked: newChecked
-    });
-  }
   componentDidMount() {
-    window.scrollTo(0, 0);
-    document.body.scrollTop = 0;
+/*     window.scrollTo(0, 0);
+    document.body.scrollTop = 0; */
+
+    this.checkIfFranchiseExists();
+  }
+
+  componentDidUpdate() {
+    // console.log(`FranchiseInfo componentDidUpdate props.franchiseId: ${this.props.franchiseId}`);
+    if (this.state.isModalUpdated) {
+      this.setState({isModalUpdated: false, submittingFranchise: false});
+      this.getFranchiseInfo(this.props.franchiseId);
+    }
+    if (this.state.franchiseInfoUpdated) {
+      this.setState({franchiseInfoUpdated: false,});
+      this.getFranchiseInfo(this.props.franchiseId);
+    }
+  }
+
+  // this callback function lets us know whether a user type was changed in
+  // the 'FranchiseCreatedModal' component
+  // ========================================================================
+  getModalResult = (obj) => this.setState(obj);
+
+  // this callback function lets us know whether a franchise info item
+  // was changed in the 'FranchiseUpdateScreen' component
+  // ========================================================================
+  getUpdateResult = (obj) => this.setState(obj);
+
+  checkIfFranchiseExists() {
+    // check admin user's franchise status , find out if one exists.
+    const {franchiseId, franchiseName} = this.props;
+    // console.log(`FranchiseInfo checkIfFranchiseExists() franchiseId: ${franchiseId} franchiseName: ${franchiseName}`);
+
+    if (franchiseId !== "" && franchiseId !== "undefined") {
+      this.setState({doesFranchiseExist: true});
+    }
+  }
+
+  getFranchiseInfo(id) {
+    // console.log(`FranchiseInfo getFranchiseInfo() hello id: ${id}`);
+    API
+    .getFranchise(id)
+    .then(res => {
+      const franchiseInfo = res.data;
+      let franchise = {
+        name: franchiseInfo.franchiseName,
+        address: franchiseInfo.address,
+        phone: franchiseInfo.phoneNumber,
+        email: franchiseInfo.franchiseEmail,
+      }
+      // console.log(`in FranchiseInfo() API.getFranchise franchise: ${JSON.stringify(franchise)}`);
+      this.props.franchiseInfo({franchise});
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
   
   handleChange = (event) => {
-    const { user } = this.state;
-    user[event.target.name] = event.target.value;
-    this.setState({...this.state, user });
+    const { franchise } = this.state;
+    // console.log(`state franchise: ${franchise}FranchiseInfo handleChange: name: ${event.target.name}, value: ${event.target.value}`);
+    franchise[event.target.name] = event.target.value;
+    this.setState({...this.state, franchise });
+  }
+
+  handleSubmit = () => {
+    // const {franchise} = this.state;
+    // console.log(`in handlesubmit franchise: ${JSON.stringify(franchise)}`);
+    // post franchise
+    const postObj = {
+      franchiseName: this.state.franchise.name,
+      address: this.state.franchise.address,
+      phoneNumber: this.state.franchise.phone,
+      franchiseEmail: this.state.franchise.email,
+    };
+
+    API
+    .postFranchise(postObj)
+    .then(res => {
+      // console.log(`FranchiseInfo handleSubmit res.data: ${JSON.stringify(res.data)}`);
+      this.props.franchiseInfo({
+        franchise_id: res.data._id,
+        ...this.state
+      });
+      this.setState({doesFranchiseExist: true, submittingFranchise: true});
+    })
+    .catch(err => {
+      console.log(`error: ${err}`);
+    })
   }
 
   render() {
-    const { classes, ...rest } = this.props;
-    return (
-      <GridContainer justify="center">
-      <GridItem xs={12} sm={5} md={5}>
-      <div className={classes.textCenter}>
-        <h3 className={classes.socialTitle}>
-          Enter Franchise Info
-        </h3>
-      </div>
-      <form className={classes.form}>
-        <CustomInput
-          formControlProps={{
-            fullWidth: true,
-            className: classes.customFormControlClasses
-          }}
-          inputProps={{
-            startAdornment: (
-              <InputAdornment
-                position="start"
-                className={classes.inputAdornment}
-              >
-                <Business
-                  className={classes.inputAdornmentIcon}
-                />
-              </InputAdornment>
-            ),
-            placeholder: "Business Name..."
-          }}
-        />
+    const { classes, franchiseId, franchiseName, franchiseAddress, franchisePhone, franchiseEmail,} = this.props;
+    // console.log("FranchiseInfo render()", JSON.stringify(this.props));
 
-        <CustomInput
-          formControlProps={{
-            fullWidth: true,
-            className: classes.customFormControlClasses
-          }}
-          inputProps={{
-            startAdornment: (
-              <InputAdornment
-                position="start"
-                className={classes.inputAdornment}
-              >
-                <ContactMail
-                  className={classes.inputAdornmentIcon}
-                />
-              </InputAdornment>
-            ),
-            placeholder: "Address..."
-          }}
-        />
-        <CustomInput
-          formControlProps={{
-            fullWidth: true,
-            className: classes.customFormControlClasses
-          }}
-          inputProps={{
-            startAdornment: (
-              <InputAdornment
-                position="start"
-                className={classes.inputAdornment}
-              >
-                <ContactPhone
-                  className={classes.inputAdornmentIcon}
-                />
-              </InputAdornment>
-            ),
-            placeholder: "Phone number..."
-          }}
-        />
-        <CustomInput
-          formControlProps={{
-            fullWidth: true,
-            className: classes.customFormControlClasses
-          }}
-          inputProps={{
-            startAdornment: (
-              <InputAdornment
-                position="start"
-                className={classes.inputAdornment}
-              >
-                <Email
-                  className={classes.inputAdornmentIcon}
-                />
-              </InputAdornment>
-            ),
-            placeholder: "Company email..."
-          }}
-        />
+    if (!this.state.doesFranchiseExist) {
+      return (
+        <GridContainer justify="center">
+        <GridItem xs={12} sm={5} md={5}>
         <div className={classes.textCenter}>
-          <Button round color="primary">
-            Get started
-          </Button>
+          <h3 className={classes.socialTitle}>
+            Enter Franchise Info
+          </h3>
         </div>
-      </form>
-    </GridItem>
-    </GridContainer>
+        <ValidatorForm 
+          onSubmit={this.handleSubmit}
+          className={classes.form}
+        >
+          <GridItem xs={12} className={classes.vSpace}>
+            <TextValidator
+              id="franchise-name"
+              name="name"
+              label="Business name"
+              autoComplete="fname"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start" className={classes.customFormControlClasses}>
+                    <Business className={classes.inputAdornmentIcon} />
+                  </InputAdornment>
+                ),
+                placeholder: "Company name ..."
+              }}
+              validators={['required',`minStringLength:${MinCompanyLength}`, `maxStringLength:${MaxCompanyLength}`]}
+              errorMessages={['this field is required', `At least ${MinCompanyLength} characters long`, `Must be less than ${MaxCompanyLength}`]}
+              value={this.state.franchise.name}
+              onChange={this.handleChange}
+            />
+          </GridItem>
+          <GridItem xs={12} className={classes.vSpace}>
+            <TextValidator
+              id="franchise-address"
+              name="address"
+              label="Business Address"
+              autoComplete="faddress"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <ContactMail className={classes.inputAdornmentIcon} />
+                  </InputAdornment>
+                ),
+                placeholder: "Enter address ..."
+              }}
+              validators={['required',`minStringLength:${MinAddressLength}`, `maxStringLength:${MaxAddressLength}`]}
+              errorMessages={['address is required', `At least ${MinAddressLength} characters long`, `Must be less than ${MaxAddressLength}`]}
+              value={this.state.franchise.address}
+              onChange={this.handleChange}
+            />              
+          </GridItem>
+          <GridItem xs={12} className={classes.vSpace}>
+            <TextValidator
+              id="phone-number"
+              name="phone"
+              label="Phone Number"
+              autoComplete="fphone"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <ContactPhone className={classes.inputAdornmentIcon} />
+                  </InputAdornment>
+                ),
+                placeholder: "Phone number ..."
+              }}
+              validators={['required',`minStringLength:${MinPhoneLength}`, `maxStringLength:${MaxPhoneLength}`]}
+              errorMessages={['phone is required', `At least ${MinPhoneLength} characters long`, `Must be less than ${MaxPhoneLength}`]}
+              value={this.state.franchise.phone}
+              onChange={this.handleChange}
+            />              
+          </GridItem>
+          <GridItem xs={12} className={classes.vSpace}>
+            <TextValidator
+              id="company-email"
+              name="email"
+              label="Business Email"
+              autoComplete="femail"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Email className={classes.inputAdornmentIcon} />
+                  </InputAdornment>
+                ),
+                placeholder: "Email ..."
+              }}
+              validators={['required','isEmail']}
+              errorMessages={['email is required', 'Please enter valid email']}
+              value={this.state.franchise.email}
+              onChange={this.handleChange}
+            />              
+          </GridItem>
+          <div className={classes.textCenter}>
+            <Button 
+            type="submit"
+            round color="primary">
+              Submit Info
+            </Button>
+          </div>
+        </ValidatorForm>
+      </GridItem>
+      </GridContainer>
+      );
+    } 
+    if (this.state.submittingFranchise) {
+      return (
+        <FranchiseCreatedModal
+          modalTitle="Franchise Successfully Created"
+          modalContent="Business name, address, email and phone number available. You can update information later" 
+          sendModalResult={this.getModalResult}
+        />
+      );
+    }
+
+    return (
+      <FranchiseUpdateScreen
+        franchiseName={franchiseName}
+        franchiseAddress={franchiseAddress}
+        franchisePhone={franchisePhone}
+        franchiseEmail={franchiseEmail}
+        franchiseId={franchiseId}
+        sendUpdate={this.getUpdateResult}
+      />
     );
   }
 }
 
 FranchiseInfo.propTypes = {
-  labelText: PropTypes.node,
-  labelProps: PropTypes.object,
-  id: PropTypes.string,
-  inputProps: PropTypes.object,
-  formControlProps: PropTypes.object,
-  inputRootCustomClasses: PropTypes.string,
-  error: PropTypes.bool,
-  success: PropTypes.bool,
-  white: PropTypes.bool
+  classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(FranchiseInfo);
-
-/*
-
-        <TextValidator
-          id="franchise-name"
-          name="franchiseName"
-          label="Business Name"
-          fullWidth
-          autoComplete="fname"
-          validators={['required']}
-          errorMessages={['this field is required']}
-          value={this.state.franchise.name}
-          onChange={this.handleChange}
-        />
-
-*/
