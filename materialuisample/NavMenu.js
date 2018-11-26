030ea7322a0ee7f70b920ec0ec63120a351cf63d
Fabian Flores <ffflores1@outlook.com>
2018-11-25 21:23:01 -0500

    updated nav menu with more items -- added rental submenu

--- materialuisample/NavMenu.js
+++ materialuisample/NavMenu.js
@@ -4,84 +4,43 @@
 //
 // ==========================================================
 
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import {Link, withRouter} from "react-router-dom";
-
 import PropTypes from 'prop-types';
+// @material-ui/core components
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import blue from '@material-ui/core/colors/blue';
-// import green from '@material-ui/core/colors/green';
-import pink from '@material-ui/core/colors/pink';
-import { darken, lighten } from 'material-ui/styles/colorManipulator';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from '@material-ui/core/ListItemText';
+// @material-ui icons
+// import MenuIcon from '@material-ui/icons/Menu';
+import Settings from '@material-ui/icons/Settings';
+// @material core components
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Header from "components/Header/Header.jsx";
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import navbarsStyle from "assets/jss/material-kit-pro-react/views/componentsSections/navbarsStyle.jsx";
+
 import { compose } from 'recompose';
-
 import {AppName} from "../constants/Consts";
-
-const styles = {
-  root: {
-    flexGrow: 1,
-    color: 'pink',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
-
-const themeDefault = createMuiTheme();
-
-const theme = createMuiTheme({
-  overrides: {
-    MuiMenuItem: {
-      root: {
-        color: 'white',
-        fontWeight: 'bold',
-      },
-      selected: {
-        color: 'lightgray !important',
-        backgroundColor: 'darkblue !important',
-      },
-    },
-  },
-  palette: {
-    primary: { 
-      light: blue[300], 
-      main: blue[600], 
-      dark: blue[800],
-      textColor: '#fff',
-    },
-    secondary: {
-      light: lighten(pink.A400, 0.07),
-      main: pink.A400,
-      dark: darken(pink.A400, 0.07),
-      contrastText: themeDefault.palette.getContrastText(pink.A400),
-    },
-    // error: red.A400,
-  },
-});
 
 // safe measure to avoid properties collisions
 const Login = props => <Link to="/login" {...props} />
 const Home = props => <Link to="/" {...props} />
 const About = props => <Link to="/about" {...props} />
 const Dashboard = props => <Link to="/dashboard" {...props} />
+const UpdatePassword = props => <Link to="/updatepassword" {...props} />
 const Signup = props => <Link to="/signup" {...props} />
 const Logout = props => <Link to="/logout" {...props} />
 const SDashboard = props => <Link to="/superdashboard" {...props} />
 const FDashboard = props => <Link to="/fadmindashboard" {...props} />
 const EmpDashboard = props => <Link to="/edashboard" {...props} />
 const CustDashboard = props => <Link to="/cdashboard" {...props} />
-
+const MyItems = props => <Link to="/myitems" {...props} />
+const RentalItems = props => <Link to="/rentalitems" {...props} />
+const PostRental = props => <Link to="/postrental" {...props} />
+const ViewRentals = props => <Link to="/viewrentals" {...props} />
 
 // ---
 // functional component that renders nav item if logged in user is a super administrator
@@ -93,10 +52,13 @@ function SAdminBar(props) {
   // console.log(`NavMenu.js SAdminBar props.isAdmin: ${props.isAdmin}`);
   if (isSuperAdmin) {
     return (
-    <Toolbar>
-      {/* <MenuItem component={Dashboard} selected={"/dashboard" === pathname}>Admin</MenuItem> */}
-      <MenuItem component={SDashboard} selected={"/superdashboard" === pathname}>S Dashboard</MenuItem>
-    </Toolbar>
+    <ListItem
+      button
+      component={SDashboard}
+      selected={"/superdashboard" === pathname}
+    >
+      S.Dashboard
+    </ListItem>
     );
   }
   return null;
@@ -106,13 +68,15 @@ function FAdminBar(props) {
   const isFAdmin = props.isFAdmin,
         pathname = props.pathname;
 
-  // console.log(`NavMenu.js SAdminBar props.isAdmin: ${props.isAdmin}`);
   if (isFAdmin) {
     return (
-    <Toolbar>
-      {/* <MenuItem component={Dashboard} selected={"/dashboard" === pathname}>Admin</MenuItem> */}
-      <MenuItem component={FDashboard} selected={"/fadmindashboard" === pathname}>F Dashboard</MenuItem>
-    </Toolbar>
+    <ListItem
+      button
+      component={FDashboard}
+      selected={"/fadmindashboard" === pathname}
+    >
+    A.Dashboard
+    </ListItem>
     );
   }
   return null;
@@ -122,12 +86,15 @@ function EmployeeBar(props) {
   const isEmployee = props.isEmployee,
         pathname = props.pathname;
 
-  // console.log(`NavMenu.js SAdminBar props.isAdmin: ${props.isAdmin}`);
   if (isEmployee) {
     return (
-    <Toolbar>
-      <MenuItem component={EmpDashboard} selected={"/edashboard" === pathname}>Dashboard</MenuItem>
-    </Toolbar>
+    <ListItem
+      button 
+      component={EmpDashboard}
+      selected={"/edashboard" === pathname}
+    >
+      Dashboard
+    </ListItem>
     );
   }
   return null;
@@ -140,9 +107,13 @@ function CustomerBar(props) {
   // console.log(`NavMenu.js SAdminBar props.isAdmin: ${props.isAdmin}`);
   if (isCustomer) {
     return (
-    <Toolbar>
-      <MenuItem component={CustDashboard} selected={"/cdashboard" === pathname}>My Info</MenuItem>
-    </Toolbar>
+    <ListItem
+      button
+      component={CustDashboard}
+      selected={"/cdashboard" === pathname}
+    >
+      My-Info
+    </ListItem>
     );
   }
   return null;
@@ -154,82 +125,202 @@ function CustomerBar(props) {
 // ---------------------------------------------------------------------------------------
 function AuthMenu(props) {
   const isLoggedIn = props.isLoggedIn,
-        pathname = props.pathname;
+        pathname = props.pathname,
+        classes = props.classes;
   
   if (isLoggedIn) {
     return (
-      <Toolbar> 
-        <MenuItem component={Logout} selected={"/logout" === pathname}>Logout</MenuItem>
-        <MenuItem><p>{props.email}</p></MenuItem>
-      </Toolbar>
+      <Fragment>
+        <ListItem className={classes.listItem}>
+          <CustomDropdown
+            left
+            dropdownHeader="Rentals"
+            buttonText="Rentals"
+            buttonProps={{
+              style: {textTransform: 'capitalize', fontSize: '14px'},
+              color: "transparent",
+            }}
+            dropdownList={[
+              <ListItem
+                className={`${classes.listItem} ${classes.navLinkActive}`}
+                component={MyItems}
+                selected={"/myitems" === pathname}
+              >
+              My Items
+              </ListItem>,
+              <ListItem
+                className={`${classes.listItem} ${classes.navLinkActive}`}
+                component={PostRental}
+                selected={"/postrental" === pathname}
+              >
+              Post Rental
+              </ListItem>,
+              <ListItem
+                className={`${classes.listItem} ${classes.navLinkActive}`}
+                component={ViewRentals}
+                selected={"/viewrentals" === pathname}
+              >
+              View Rental
+              </ListItem>,
+              <ListItem
+                className={`${classes.listItem} ${classes.navLinkActive}`}
+                component={RentalItems}
+                selected={"/rentalitems" === pathname}        
+              >
+                About Rentals
+              </ListItem>             
+            ]}
+          />
+        </ListItem>
+        <ListItem>
+          <span>{props.email}</span>
+        </ListItem>
+        <ListItem className={classes.listItem}>
+          <CustomDropdown
+            left
+            dropdownHeader="Settings"
+            buttonIcon={Settings}
+            buttonProps={{
+              className: classes.navLink,
+              color: "transparent"
+            }}
+            dropdownList={[
+              <ListItem
+                className={`${classes.listItem} ${classes.navLinkActive}`}
+                component={UpdatePassword}
+                selected={"/updatepassword" === pathname}
+              >
+              Change password
+              </ListItem>,              
+              "Action 1",
+              "Another action",
+              { divider: true },
+              <ListItem
+                className={`${classes.listItem} ${classes.navLinkActive}`}
+                component={Logout}
+                selected={"/logout" === pathname}
+              >
+              Logout
+              </ListItem>, 
+            ]}
+          />
+        </ListItem>
+      </Fragment>
     );
   }
 
   return (
-   <Toolbar>
-      <MenuItem component={Login} selected={"/login" === pathname}>Login</MenuItem>
-      <MenuItem component={Signup} selected={"/signup" === pathname}>Sign Up</MenuItem>
-   </Toolbar>
+    <Fragment>
+      <ListItem
+        button 
+        component={Login} 
+        selected={"/login" === pathname}
+      >
+        Login
+      </ListItem>
+      <ListItem
+        button
+        component={Signup} 
+        selected={"/signup" === pathname}
+      >
+        Signup
+      </ListItem>
+   </Fragment>
   );
 }
 
 
 
 class NavMenu extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      auth: true,
-      anchorEl: null,   
-    };
-
+  componentWillReceiveProps() {
+    // const { location: {pathname}, history } = this.props;
+    // history.push(pathname);
   }
-
 
   render() {
     const { classes, location: {pathname} } = this.props;
 
     return  (
-      <MuiThemeProvider theme={theme}>
-      <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="title" color="inherit" className={classes.grow}>
-            {AppName}
-          </Typography>
-          <MenuItem color="inherit" component={Home} selected={"/" === pathname}>Home</MenuItem>
-          <MenuItem color="inherit" component={About} selected={"/about" === pathname}>About</MenuItem>
-          { !this.props.isLoggedIn &&
-          <MenuItem color="inherit" component={Dashboard} selected={"/dashboard" === pathname}>Dashboard</MenuItem>
-          }
-          <SAdminBar 
-            isSuperAdmin = {this.props.isSuperAdmin} 
-            pathname = {pathname}
-          />
-          <FAdminBar 
-            isFAdmin = {this.props.isFAdmin} 
-            pathname = {pathname}
-          />
-          <EmployeeBar 
-            isEmployee = {this.props.isEmployee} 
-            pathname = {pathname}
-          />
-          <CustomerBar 
-            isCustomer = {this.props.isCustomer} 
-            pathname = {pathname}
-          />
+      <div className={`cd-section`} id="navigation">
+      <div className={classes.container}>
+        <GridContainer>
+          <GridItem xs={12}>
+            <Header
+            brand={AppName}
+            color={ (pathname === "/") ? "transparent" : "info"}
+            fixed={(pathname === "/") ? true : false}
+            changeColorOnScroll={(pathname === "/") 
+              ? {
+                  height: 300,
+                  color: "info",
+                } 
+              : {
+                  height: 0,
+                  color: "transparent",
+                }
+            }
+            links={
+              <List className={`${classes.list} ${classes.mlAuto}`}>
+                <ListItem 
+                  button
+                  color="transparent"
+                  component={Home}
+                  selected={"/" === pathname}
+                >
+                Home
+                </ListItem>
+                <ListItem 
+                  button
+                  color="transparent"
+                  component={About}
+                  selected={"/about" === pathname}
+                >
+                About
+                </ListItem>
+                { !this.props.isLoggedIn &&
+                  <ListItem 
+                    button
+                    color="transparent"
+                    component={Dashboard}
+                    selected={"/dashboard"===pathname}
+                  > 
+                    Dashboard
+                  </ListItem>
+                } 
 
-          <AuthMenu 
-            isLoggedIn = {this.props.isLoggedIn}
-            email = {this.props.email}
-            pathname = {pathname}
+                <SAdminBar
+                  isSuperAdmin = {this.props.isSuperAdmin} 
+                  pathname = {pathname}
+                  classes = {classes}
+                />
+                <FAdminBar 
+                  isFAdmin = {this.props.isFAdmin} 
+                  pathname = {pathname}
+                  classes = {classes}
+                />
+                <EmployeeBar 
+                  isEmployee = {this.props.isEmployee} 
+                  pathname = {pathname}
+                  classes = {classes}
+                />
+                <CustomerBar 
+                  isCustomer = {this.props.isCustomer} 
+                  pathname = {pathname}
+                  classes = {classes}
+                />   
+                <AuthMenu
+                  isLoggedIn = {this.props.isLoggedIn}
+                  email = {this.props.email}
+                  pathname = {pathname}
+                  classes = {classes}
+                />
+              </List>
+            }
           />
-        </Toolbar>
-      </AppBar>
-    </div>
-    </MuiThemeProvider>
+          </GridItem>
+        </GridContainer>
+      </div>
+      </div>
     );
   }
 }
@@ -240,13 +331,7 @@ NavMenu.propTypes = {
 
 export default compose (
   withRouter,
-  withStyles(styles)
+  withStyles(navbarsStyle)
 )(NavMenu);
 
-/*
-
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-
-*/
+// className={`${classes.listItem} ${classes.navLink} ${classes.navLinkActive}`} 
