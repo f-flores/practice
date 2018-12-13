c6bf185c409cea54295048a2a115459149e3c02d
Fabian Flores <ffflores1@outlook.com>
2018-12-12 20:17:26 -0500

    added sum function

--- problem-solving/sums2.js
+++ problem-solving/sums2.js
@@ -25,7 +25,18 @@ function sums2(a) {
   }
 }
 
+function sumRecursive(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  else {
+    return n + sumRecursive(n-1);
+  }
+
+}
+
 console.log(sums(2, 3));
 console.log(sums((2), (3)));
 console.log(sums2(2) (3));
 console.log(mult(2) (3) (4));
+console.log(sumRecursive(5000));
