6d5bf1e18a7099efca069dbcee11ef3074cc308e
Fabian Flores <ffflores1@outlook.com>
2019-03-26 21:24:18 -0400

    removed undefined comments

--- problem-solving/firstDups.js
+++ problem-solving/firstDups.js
@@ -1,7 +1,6 @@
 const arr = [1, 3, 5, 7, 3, 9, 8];
-undefined
 var dups = {};
-undefined
+
 function getFirstDup(arr) {
     for (let i = 0; i < arr.length; i++){
         if (dups[arr[i]] === undefined) {
